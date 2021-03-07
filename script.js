cb1a38b3efa70d943ecd0e6fe838cbd09cbde454
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-06 22:32:54 -0500

    Display time left

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
+  // Clear any existing timers
+  clearInterval(countdown);
+  
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then); 
+
   // Display how many seconds left
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -26,7 +32,12 @@ function displayTimeLeft(seconds) {
   timerDisplay.textContent = display;
 }
 
-timer(125);
-
-
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours(); 
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
  
+
+timer(124)
